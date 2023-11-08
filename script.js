e4e1c6640af59e576f9f1a832645758d63757993
John M <entity.john@gmail.com>
2023-11-08 15:14:17 -0800

    show player image, aim it around planet at mouse

--- script.js
+++ script.js
@@ -25,17 +25,26 @@ class Player {
     this.x = this.game.width * 0.5;
     this.y = this.game.height * 0.5;
     this.radius = 40; // player and monsters are 40px radius, rectangular sprites are 80x80, radius of planet is 80
+    this.image = document.getElementById("player");
+    this.aim;
   }
   draw(context) {
-    /* 
-        Draw the player as a circle
-    */
+    context.drawImage(this.image, this.x - this.radius, this.y - this.radius);
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.stroke();
   }
-  update() {}
+  update() {
+    this.aim = this.game.calcAim(this.game.mouse, this.game.planet);
+    this.x =
+      this.game.planet.x +
+      (this.game.planet.radius + this.radius) * this.aim[0];
+    this.y =
+      this.game.planet.y +
+      (this.game.planet.radius + this.radius) * this.aim[1];
+  }
 }
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
@@ -63,6 +72,14 @@ class Game {
     context.moveTo(this.planet.x, this.planet.y);
     context.lineTo(this.mouse.x, this.mouse.y);
     context.stroke();
+  }
+  calcAim(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const aimX = dx / distance;
+    const aimY = dy / distance;
+    return [aimX, aimY, dx, dy];
   }
 }
 
