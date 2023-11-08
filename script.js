5acd9b98a914e0b8e4a19b7a4c8ad3af7997909f
John M <entity.john@gmail.com>
2023-11-08 13:17:10 -0800

    draw player as a circle

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 class Planet {
   constructor(game) {
     this.game = game;
-    this.x = game.width * 0.5;
-    this.y = game.height * 0.5;
-    this.radius = 80; // sprites are 80x80, radius of planet is 80
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
+    this.radius = 80;
     this.cloudRadius = 100; // the clouds of the planet stick out 20px, but will not count towards sprite collisions
     this.image = document.getElementById("planet");
   }
@@ -19,12 +19,30 @@ class Planet {
   }
 }
 
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
+    this.radius = 40; // player and monsters are 40px radius, rectangular sprites are 80x80, radius of planet is 80
+  }
+  draw(context) {
+    /* 
+        Draw the player as a circle
+    */
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.stroke();
+  }
+  update() {}
+}
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.planet = new Planet(this);
+    this.player = new Player(this);
     this.mouse = {
       x: 0,
       y: 0,
@@ -36,6 +54,8 @@ class Game {
   }
   render(context) {
     this.planet.draw(context);
+    this.player.draw(context);
+    this.player.update();
     /* 
         draws a line from the planet to the mouse
     */
